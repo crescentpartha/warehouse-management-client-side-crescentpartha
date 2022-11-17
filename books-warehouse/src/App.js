93be618e2cff3a99f25bc8094b34ffdfe16dcbb2
crescentpartha <crescentpartha406@gmail.com>
2022-11-18 02:48:36 +0600

    Implement the responsive navbar section using tailwind-css

--- books-warehouse/src/App.js
+++ books-warehouse/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/not-found' element={<NotFound></NotFound>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- books-warehouse/src/components/SharedPages/Header/Header.js
+++ books-warehouse/src/components/SharedPages/Header/Header.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from '../../../images/logo.png';
+import { FaBars } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
+
     return (
-        <div>
-            <h2 className="text-3xl font-bold underline text-orange-500">Header</h2>
-        </div>
+        <header className='border-b border-gray-300 py-2'>
+            <div className='flex items-center justify-between container px-4 mx-auto flex-wrap w-full'>
+                <div>
+                    <Link className='flex items-center justify-between' to='/'>
+                        <img className='w-9 mr-2' src={logo} alt="logo" />
+                        <p className='text-lg font-semibold'>Books Warehouse</p>
+                    </Link>
+                </div>
+
+                <FaBars className='lg:hidden block h-6 w-6 cursor-pointer' onClick={() => setOpen(!open)} />
+
+                <nav className={`${open ? "block" : "hidden"} lg:flex lg:items-center lg:w-auto w-full`}>
+                    <ul className='lg:flex lg:items-center lg:justify-between text-base text-gray-700'>
+                        <li>
+                            <Link className='lg:px-5 py-2 font-semibold hover:text-blue-700 block' to="/home">Home</Link>
+                        </li>
+                        <li>
+                            <Link className='lg:px-5 py-2 font-semibold hover:text-blue-700 block' to="/manage">Manage</Link>
+                        </li>
+                        <li>
+                            <Link className='lg:px-5 py-2 font-semibold hover:text-blue-700 block' to="/about">Blogs</Link>
+                        </li>
+                        <li>
+                            <Link className='lg:px-6 py-2 bg-blue-700 hover:bg-blue-600 text-white rounded-full block text-center' to="/login">Login</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     );
 };
 
