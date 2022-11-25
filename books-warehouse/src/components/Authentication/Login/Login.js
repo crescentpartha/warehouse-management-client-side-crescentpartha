a032532906ac4c1d3202d9e34736ec0e30d40de2
crescentpartha <crescentpartha406@gmail.com>
2022-11-25 19:55:59 +0600

    Implement Google Login System from react-firebase-hooks for SocialLogin

--- books-warehouse/src/components/Authentication/Login/Login.js
+++ books-warehouse/src/components/Authentication/Login/Login.js
@@ -3,7 +3,7 @@ import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
-import google from '../../../images/google.png';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
@@ -31,16 +31,7 @@ const Login = () => {
         <div className='bg-gray-200 w-60 sm:w-80 mx-auto my-10 py-5 rounded'>
             <h2 className='text-blue-500 text-2xl font-semibold'>Login your account</h2>
 
-            <button className='flex items-center justify-center gap-4 bg-blue-400 hover:bg-blue-500 py-2 mx-auto my-4 rounded w-10/12'>
-                <img width={25} src={google} alt="google logo" />
-                <span className='text-white text-lg font-semibold uppercase'>Google</span>
-            </button>
-
-            <div className='flex flex-row flex-nowrap items-center justify-center mx-5'>
-                <hr width='44%' className='h-1 bg-slate-50' />
-                <p className='mx-2 text-gray-50 text-lg font-semibold'>or</p>
-                <hr width='44%' className='h-1 bg-slate-50' />
-            </div>
+            <SocialLogin></SocialLogin>
 
             <form className='flex flex-col gap-2 p-5' onSubmit={handleSubmit(onSubmit)}>
                 <input
---
+++ books-warehouse/src/components/Authentication/SocialLogin/SocialLogin.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import google from '../../../images/google.png';
+
+const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+
+    if (user) {
+        navigate('/home');
+    }
+
+    return (
+        <div>
+            <button onClick={() => signInWithGoogle()} className='flex items-center justify-center gap-4 bg-blue-400 hover:bg-blue-500 py-2 mx-auto my-4 rounded w-10/12'>
+                <img width={25} src={google} alt="google logo" />
+                <span className='text-white text-lg font-semibold uppercase'>Google</span>
+            </button>
+            
+            { error && <p className='text-red-400'>{error.message}</p>}
+            { loading && <p className='text-red-400'>Loading...</p>}
+
+            <div className='flex flex-row flex-nowrap items-center justify-center mx-5'>
+                <hr width='44%' className='h-1 bg-slate-50' />
+                <p className='mx-2 text-gray-50 text-lg font-semibold'>or</p>
+                <hr width='44%' className='h-1 bg-slate-50' />
+            </div>
+        </div>
+    );
+};
+
+export default SocialLogin;
