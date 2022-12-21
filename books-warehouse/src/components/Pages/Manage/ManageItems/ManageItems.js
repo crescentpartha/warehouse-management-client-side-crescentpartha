1428355d49e5461c7940d4d3a3c2f5575603bf94
crescentpartha <crescentpartha406@gmail.com>
2022-12-21 23:55:35 +0600

    Get page count and Create pagination buttons

--- books-warehouse/src/components/Pages/Manage/ManageItems/ManageItems.js
+++ books-warehouse/src/components/Pages/Manage/ManageItems/ManageItems.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import useBooks from '../../../../hooks/useBooks';
 import DisplayManageItems from '../DisplayManageItems/DisplayManageItems';
 
 const ManageItems = () => {
     const [books] = useBooks();
+    const [pageCount, setPageCount] = useState(0);
+
+    useEffect( () => {
+        fetch('http://localhost:5000/bookCount')
+        .then(res => res.json())
+        .then(data => {
+            const count = data.count;
+            const pages = Math.ceil(count/10);
+            setPageCount(pages);
+        })
+    }, []);
     return (
         <div className='p-5 my-5 border-bottom w-screen'>
             <h2 className='text-4xl sm:text-5xl font-normal uppercase pb-10'>Manage Items</h2>
@@ -33,6 +44,14 @@ const ManageItems = () => {
                         }
                     </tbody>
                 </table>
+            </div>
+            <div className='mt-10'>
+                {
+                    [...Array(pageCount).keys()]
+                    .map(number => <button 
+                        className='bg-gray-200 border-2 border-orange-300 rounded px-3 py-2 mx-1'
+                    >{number + 1}</button>)
+                }
             </div>
             <div className='mb-5 mt-10'>
                 <Link className='bg-blue-700 hover:bg-blue-600 px-5 py-2 rounded text-white' to='/add-item'>Add Items</Link>
